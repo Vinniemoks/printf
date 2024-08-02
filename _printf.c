48bee82acf1f13aa6a735015b181b37e79c1dddb
Vincent Mokua <mokua.vinny@gmail.com>
2024-08-02 02:14:37 -0700

    tf!

--- _printf.c
+++ _printf.c
@@ -3,8 +3,8 @@
 void print_buffer(char buffer[], int *buff_ind);
 
 /**
- * _printf - Printf function for output
- * @format: format that is used
+ * _printf - Printf function
+ * @format: format.
  * Return: Printed chars.
  */
 int _printf(const char *format, ...)
@@ -52,3 +52,15 @@ int _printf(const char *format, ...)
 	return (printed_chars);
 }
 
+/**
+ * print_buffer - Prints the contents of the buffer if it exist
+ * @buffer: Array of chars
+ * @buff_ind: Index at which to add next char, represents the length.
+ */
+void print_buffer(char buffer[], int *buff_ind)
+{
+	if (*buff_ind > 0)
+		write(1, &buffer[0], *buff_ind);
+
+	*buff_ind = 0;
+}
